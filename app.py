94224a9fee1ac289a8c00d3058d03f48c4c6c596
CEDRIC <cedricpapin1@orange.fr>
2024-02-26 18:36:59 +0100

    comparison with real answer

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import duckdb
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-# solution_df = duckdb.sql(ANSWER_STR).df()
-
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to review ?",
@@ -20,25 +18,31 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
 st.header("Entrez votre code : ")
 query = st.text_area(label="Renseignez votre code SQL ici : ", key="user_input")
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
-#
-#    try:
-#        result = result[solution_df.columns]
-#        st.dataframe(result.compare(solution_df))
-#    except KeyError as e:
-#        st.write("Some columns are missing")
-#
-#    n_lines_difference = result.shape[0] - solution_df.shape[0]
-#    if n_lines_difference != 0:
-#        st.write(
-#            f"result has a {n_lines_difference} lines difference with the solution"
-#        )
-#
-#
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution"
+        )
+
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
@@ -49,7 +53,4 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
     st.write(answer)
