b9a731e1cb27be50f8149adf934b5f8014fdc72e
CEDRIC <cedricpapin1@orange.fr>
2024-02-28 17:26:04 +0100

    test_create_database_with_new_branch

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 # pylint: disable=missing-module-docstring
 
-import ast
-import streamlit as st
 import duckdb
+import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
@@ -15,42 +14,48 @@ with st.sidebar:
     )
     st.write("You selected :", theme)
 
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
-    st.write(exercise)
+    if theme:
+        exercise = con.execute(
+            f"SELECT * FROM memory_state WHERE theme = '{theme}'"
+        ).df().sort_values("last_reviewed").reset_index()
+        st.write(exercise)
 
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
+        exercise_name = exercise.loc[0, "exercise_name"]
+        with open(f"answers/{exercise_name}.sql", "r", encoding="utf-8") as f:
+            answer = f.read()
 
-    solution_df = con.execute(answer).df()
+        solution_df = con.execute(answer).df()
 
 st.header("Entrez votre code : ")
-query = st.text_area(label="Renseignez votre code SQL ici : ", key="user_input")
+query = st.text_area(key="user_input", label="Renseignez votre code SQL ici : ")
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
 
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
+    if theme:
+        try:
+            result = result[solution_df.columns]
+            st.dataframe(result.compare(solution_df))
+        except KeyError as e:
+            st.write("Some columns are missing")
 
-    n_lines_difference = result.shape[0] - solution_df.shape[0]
-    if n_lines_difference != 0:
-        st.write(
-            f"result has a {n_lines_difference} lines difference with the solution"
-        )
+        n_lines_difference = result.shape[0] - solution_df.shape[0]
+        if n_lines_difference != 0:
+            st.write(
+                f"result has a {n_lines_difference} lines difference with the solution"
+            )
 
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
-    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
-    for table in exercise_tables:
-        st.write(f"table : {table}")
-        df_table = con.execute(f"SELECT * FROM {table}").df()
-        st.dataframe(df_table)
+    if theme:
+        exercise_tables = exercise.loc[0, "tables"]
+        for table in exercise_tables:
+            st.write(f"table : {table}")
+            df_table = con.execute(f"SELECT * FROM {table}").df()
+            st.dataframe(df_table)
 
 with tab3:
-    st.write(answer)
+    if theme:
+        st.write(answer)
